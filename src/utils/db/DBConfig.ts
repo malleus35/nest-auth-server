6f0f6f3dd3ec24cab83c6cac721e4da119c267d4
malleus35 <malleus35@gmail.com>
2021-02-08 17:53:26 +0900

    add ormconfig.json and changing DBConfig object not to be singleton

--- src/utils/db/DBConfig.ts
+++ src/utils/db/DBConfig.ts
@@ -1,43 +1,10 @@
-class DBConfig {
-  private static instance: DBConfig;
+import ModuleInfo from '../adapter/ModuleInfo';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-  private readonly type: string;
-  private readonly host: string;
-  private readonly port: number;
-  private readonly username: string;
-  private readonly password: string;
-  private readonly database: string;
-  private readonly entities: Array<unknown>;
-  private synchronize: boolean;
-
+class DBConfig extends ModuleInfo {
   private constructor() {
-    this.type = process.env.DB_TYPE;
-    this.host = process.env.DB_HOST;
-    this.port = Number(process.env.DB_PORT);
-    this.username = process.env.DB_USERNAME;
-    this.password = process.env.DB_PASSWORD;
-    this.database = process.env.DATABASE;
-    this.entities = [];
-    this.synchronize = true;
-  }
-
-  private static getSingleton(): void {
-    if (DBConfig.instance == null) {
-      DBConfig.instance = new this();
-    }
-  }
-
-  public static getInstance(): DBConfig {
-    if (DBConfig.instance == null) DBConfig.getSingleton();
-    return DBConfig.instance;
-  }
-
-  public pushToEntities(entity: unknown): void {
-    this.entities.push(entity);
-  }
-
-  public changeSyncronize(): void {
-    this.synchronize = this.synchronize ? false : true;
+    super();
+    this.pushToImports(TypeOrmModule.forFeature());
   }
 }
 
